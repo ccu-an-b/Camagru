01b62ec5911d2e0a5348e1c106e8dde01bbac404
Chloe CU AN BINH <ccu-an-b@student.42.fr>
2018-08-01 18:17:35 +0200

    extras

--- connexion.php
+++ connexion.php
@@ -9,7 +9,7 @@ if ((empty($_POST['login']) || empty($_POST['passwd'])) && isset($_POST['submit'
 	$_SESSION['error'] = "Champs incomplets";
 else if (isset($_POST['login']) && isset($_POST['passwd']) && isset($_POST['submit']) )
 	if(ft_user_check($_POST['login'], $_POST['passwd']))
-		header('Location: ./profile.php');
+		header('Location: ./camera.php');
 
 $error = ft_error();
 
